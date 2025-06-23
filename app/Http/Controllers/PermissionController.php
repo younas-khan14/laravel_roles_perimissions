4ea197ea5a28f69957ef8b460984c475cb5763c2
younas-khan14 <myounas.ce@gmail.com>
2025-06-23 15:32:48 +0500

    Permission Create implemented

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -3,8 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Spatie\Permission\Models\Permission;
 
 class PermissionController extends Controller
 {
-    //
+    // this method will show permissions page
+    public function index()
+    {
+        return view('permissions.list');
+    }
+    public function create()
+    {
+        return view('permissions.create');
+    }
+    public function store(Request $request)
+    {
+        // dd($request->all());
+        $validator = Validator::make($request->all(),[
+            'name'=>'required|unique:permissions|min:3'
+        ]);
+        if($validator->passes())
+        {
+            Permission::create(['name'=>$request->name]);
+            return redirect()->route('permissions.index')->with('success','Permission added successfully.');
+        }
+        else
+        {
+            return redirect()->route('permissions.create')->withInput()->withErrors($validator);
+        }
+
+    }
+    public function edit()
+    {
+        
+    }
+    public function update()
+    {
+        
+    } 
+    public function destroy()
+    {
+        
+    }     
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PermissionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +16,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/permissions', [PermissionController::class, 'index'])->name('permissions.index');
+    Route::get('/permissions/create', [PermissionController::class, 'create'])->name('permissions.create');
+    Route::post('/permissions', [PermissionController::class, 'store'])->name('permissions.store');
 });
 
 require __DIR__.'/auth.php';
